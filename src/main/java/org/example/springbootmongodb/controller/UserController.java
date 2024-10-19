30a2e17aa66062620d0871dd9de12aa7e4a5f90c
ricardo.roque <ricardo.roque@neomind.com.br>
2024-10-19 10:09:14 -0300

    Obtendo um usuário por id

--- src/main/java/org/example/springbootmongodb/controller/UserController.java
+++ src/main/java/org/example/springbootmongodb/controller/UserController.java
@@ -6,6 +6,7 @@ import org.example.springbootmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,12 @@ public class UserController {
         List<User> list = userService.findAll();
         List<UserDTO> listDTO = list.stream().map(user -> new UserDTO(user.getId(), user.getName(), user.getEmail())).collect(Collectors.toList());
         return ResponseEntity.ok(listDTO);
+    }
+
+    @GetMapping("/findById/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User user = userService.findById(id);
+        UserDTO userDTO = new UserDTO(user.getId(), user.getName(), user.getEmail());
+        return ResponseEntity.ok(userDTO);
     }
 }
