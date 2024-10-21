e95bca84affca47a7edf5a66732a4446b4739a46
ricardo.roque <ricardo.roque@neomind.com.br>
2024-10-20 23:22:04 -0300

    Consultas utilizando o @Query

--- src/main/java/org/example/springbootmongodb/services/PostService.java
+++ src/main/java/org/example/springbootmongodb/services/PostService.java
@@ -6,6 +6,7 @@ import org.example.springbootmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -14,15 +15,20 @@ public class PostService {
     @Autowired
     private PostRepository postRepository;
 
-    public List<Post> findAll() {
-        return postRepository.findAll();
-    }
-
     public Post findById(String id) {
         Post post = postRepository.findById(id).orElse(null);
         if (post == null) {
             throw new ObjectNotFoundException("Objeto não encontrado");
         }
         return post;
+    }
+
+    public List<Post> findByTitle(String text) {
+        return postRepository.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return postRepository.fullSearch(text, minDate, maxDate);
     }
 }
